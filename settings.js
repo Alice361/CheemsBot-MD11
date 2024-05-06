f68d8ec59c6709a9750dbb3f4a25ad70e4a9fb96
kwendo204 <160314280+kwendo204@users.noreply.github.com>
2024-05-06 07:10:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
-//base by DGXeon
+//base by Wagwans
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
+//YouTube: @Wagwans
 //Instagram: unicorn_xeon13
 //Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//GitHub: @Wagwans
+//WhatsApp: +254717407351
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@Wagwans
 
 const fs = require('fs')
 const chalk = require('chalk')
@@ -16,22 +16,22 @@ global.sessionid ='ur session id'
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Nairobi,Kenya" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = '🦄💋KWENDOS BOT🦄🦄👀' //ur bot name
+global.ownernumber = '254717407351' //ur owner number
+global.ownername = '🦄드림 가이 KWENDO' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wagc = "https://chat.whatsapp.com/ECskg7L0Dn4HAr7XFh2Trp"
+global.themeemoji = '🪀👀'
+global.wm = "Wagwans Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 KWENDO\n\n+254717407351"
+global.creator = "254717407351@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["254717407351"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -41,7 +41,7 @@ global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
