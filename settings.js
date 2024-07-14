20acd6badaa5028c08794775d4627ef6e9341028
Gustianputra <putragustian212@gmail.com>
2024-07-15 03:57:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'Yanzz×Najwaa MD V11' //ur bot name
+global.ownernumber = '6283862145761' //ur owner number
+global.ownername = 'Yanzztzy' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
@@ -31,7 +31,7 @@ global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["6283131220024"] // Premium User
 global.hituet = 0
 
 //bot sett
